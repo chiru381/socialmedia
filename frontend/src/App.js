dfa677536cceb0e038477f4944200b997c45a09c
chiru381 <chirukosanam123@gmail.com>
2022-05-23 07:34:40 +0530

    social media code

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Home from './pages/home/Home';
+import Profile from './pages/profile/Profile';
+import Login from './pages/login/Login';
+import Register from './pages/register/Register';
+
+function App() {
+  return (
+    <>
+      {/* <Home /> */}
+      <Profile />
+    </>
+  );
+}
+
+export default App;
